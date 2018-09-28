71e6fa59847c30abc532dc626a0383553cc9d443
Steve Wainstead <swain@panix.com>
2018-09-27 23:55:47 -0400

    Cleaned up main script some more, after Pyflakes

--- chessercise.py
+++ chessercise.py
@@ -1,25 +1,20 @@
 import argparse
-from pieces import Board, create_piece, Knight, Queen, Rook
-
+from pieces import Board, create_piece
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-piece', dest='piece', required=True,
                     help='The piece to move')
 parser.add_argument('-position', dest='position', required=True,
                     help='The starting position of the piece')
-
 args = parser.parse_args()
 
 position = args.position.lower()
-
 piece = create_piece(args.piece.lower())
 
-print("{} and {}".format(piece.name, position))
-
 board = Board()
-
 board.place_piece(piece, position)
-possible_moves = board.list_possible_moves(piece)
-print("Possible moves:")
-print(", ".join(possible_moves))
 
+possible_moves = board.list_possible_moves(piece)
+
+print("Moves for {} at {}:".format(piece.name, position))
+print(", ".join(possible_moves))
