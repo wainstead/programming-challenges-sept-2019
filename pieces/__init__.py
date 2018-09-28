78a58a7801b3166c7a4b8cd8d30ec62c9bad1d68
Steve Wainstead <swain@panix.com>
2018-09-28 00:00:20 -0400

    Cleanup, reorg of the 'pieces' module __init__.py

--- pieces/__init__.py
+++ pieces/__init__.py
@@ -3,17 +3,28 @@ from .knight import Knight
 from .queen import Queen
 from .rook import Rook
 
-piece_map = {
-    'rook': Rook,
-    'knight': Knight,
-    'queen': Queen,
-}
 
 class PieceNotFoundError(Exception): pass
 
 
 def create_piece(piece):
-    """Factory function to create pieces."""
+    """Factory function to create pieces.
+
+    Input: piece, a string like "queen"
+
+    Output: an instance of a subclass of Piece
+
+    Raises exception if the string "piece" is not a known piece.
+
+    """
+
+    piece_map = {
+        'rook': Rook,
+        'knight': Knight,
+        'queen': Queen,
+    }
+
     if piece not in piece_map:
         raise PieceNotFoundError()
+
     return piece_map[piece]()
