d47a5ebefbee431d494796a1550e98d38c39d52f
Steve Wainstead <swain@panix.com>
2018-09-27 23:41:10 -0400

    Removed unused variable

--- pieces/rook.py
+++ pieces/rook.py
@@ -4,7 +4,6 @@ class Rook(Piece):
     name = 'Rook'
 
     def moves_algorithm(self, coordinates):
-        moves_coordinates = list()
         (row, col) = coordinates
         horizontal = [(r, col) for r in range(8) if r != row]
         vertical = [(row, c) for c in range(8) if c != col]
